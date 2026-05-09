2734c432e110b2853a5311655bf32ae2b8def4d5
SuperN64 <n64@cirnoslab.me>
2026-05-09 13:27:29 +0700

    explicitly target java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
     implementation("com.google.code.gson:gson:2.13.2")
     compileOnly("org.jetbrains:annotations:26.1.0")
